3e25157d860a5aa2a168152e32f2c3983f80571f
Brian Voong <bhlvoong@gmail.com>
2016-02-20 12:04:03 -0800

    Completed project

---
+++ facebookfeed2/ViewController.swift
@@ -0,0 +1,197 @@
+//
+//  ViewController.swift
+//  facebookfeed2
+//
+//  Created by Brian Voong on 2/20/16.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+
+let cellId = "cellId"
+
+class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Facebook Feed"
+        
+        collectionView?.alwaysBounceVertical = true
+        
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        
+        collectionView?.registerClass(FeedCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(view.frame.width, 400)
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        
+        collectionView?.collectionViewLayout.invalidateLayout()
+    }
+    
+}
+
+class FeedCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Mark Zuckerberg", attributes: [NSFontAttributeName: UIFont.boldSystemFontOfSize(14)])
+        
+        attributedText.appendAttributedString(NSAttributedString(string: "\nDecember 18  •  San Francisco  •  ", attributes: [NSFontAttributeName: UIFont.systemFontOfSize(12), NSForegroundColorAttributeName:
+            UIColor.rgb(155, green: 161, blue: 161)]))
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 4
+        
+        attributedText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, attributedText.string.characters.count))
+        
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(named: "globe_small")
+        attachment.bounds = CGRectMake(0, -2, 12, 12)
+        attributedText.appendAttributedString(NSAttributedString(attachment: attachment))
+        
+        label.attributedText = attributedText
+        
+        return label
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "zuckprofile")
+        imageView.contentMode = .ScaleAspectFit
+        return imageView
+    }()
+    
+    let statusTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Meanwhile, Beast turned to the dark side."
+        textView.font = UIFont.systemFontOfSize(14)
+        return textView
+    }()
+    
+    let statusImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "zuckdog")
+        imageView.contentMode = .ScaleAspectFill
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let likesCommentsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "488 Likes   10.7K Comments"
+        label.font = UIFont.systemFontOfSize(12)
+        label.textColor = UIColor.rgb(155, green: 161, blue: 171)
+        return label
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(226, green: 228, blue: 232)
+        return view
+    }()
+    
+    let likeButton = FeedCell.buttonForTitle("Like", imageName: "like")
+    let commentButton: UIButton = FeedCell.buttonForTitle("Comment", imageName: "comment")
+    let shareButton: UIButton = FeedCell.buttonForTitle("Share", imageName: "share")
+    
+    static func buttonForTitle(title: String, imageName: String) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, forState: .Normal)
+        button.setTitleColor(UIColor.rgb(143, green: 150, blue: 163), forState: .Normal)
+        
+        button.setImage(UIImage(named: imageName), forState: .Normal)
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0)
+        
+        button.titleLabel?.font = UIFont.boldSystemFontOfSize(14)
+        
+        return button
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.whiteColor()
+        
+        addSubview(nameLabel)
+        addSubview(profileImageView)
+        addSubview(statusTextView)
+        addSubview(statusImageView)
+        addSubview(likesCommentsLabel)
+        addSubview(dividerLineView)
+        
+        addSubview(likeButton)
+        addSubview(commentButton)
+        addSubview(shareButton)
+        
+        addConstraintsWithFormat("H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
+        
+        addConstraintsWithFormat("H:|-4-[v0]-4-|", views: statusTextView)
+        
+        addConstraintsWithFormat("H:|[v0]|", views: statusImageView)
+        
+        addConstraintsWithFormat("H:|-12-[v0]|", views: likesCommentsLabel)
+        
+        addConstraintsWithFormat("H:|-12-[v0]-12-|", views: dividerLineView)
+
+        //button constraints
+        addConstraintsWithFormat("H:|[v0(v2)][v1(v2)][v2]|", views: likeButton, commentButton, shareButton)
+        
+        addConstraintsWithFormat("V:|-12-[v0]", views: nameLabel)
+        
+        addConstraintsWithFormat("V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]-8-[v3(24)]-8-[v4(0.4)][v5(44)]|", views: profileImageView, statusTextView, statusImageView, likesCommentsLabel, dividerLineView, likeButton)
+        
+        addConstraintsWithFormat("V:[v0(44)]|", views: commentButton)
+        addConstraintsWithFormat("V:[v0(44)]|", views: shareButton)
+    }
+    
+}
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerate() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+    
+}
+
+
+
